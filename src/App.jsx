27e8c4d3a4873f7c354e61f9e527d0a38f37c405
Gikonyo-Otieno <gikonyootieno@gmail.com>
2025-09-09 02:22:28 +0300

    Rendered navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import HomePage from "./pages/HomePage";
 import Services from "./pages/Services";
 import Careers from "./pages/Careers";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* Navbar is outside Routes so it shows on every page */}
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/services" element={<Services />} />
         <Route path="/careers" element={<Careers />} />
         <Route path="/about" element={<About />} />
-
-
       </Routes>
+
+      {/* Optional: keep Footer global too */}
+      <Footer />
     </BrowserRouter>
   );
 }
